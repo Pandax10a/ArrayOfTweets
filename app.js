c6bdf380e0c4a794a51636409bcf5e40fccea516
panda1 <blueblue0011@gmail.com>
2022-07-11 21:41:43 -0700

    added 10 objects with details to the array

--- app.js
+++ app.js
@@ -1,8 +1,64 @@
+/* create an array with with multiple objects that have 4 key in each */
+/* since i am not able to access twitter, i will be using fml instead */
 let tweets = [
+    {
+        tweet: `if you've ever stolen a Tweet say sorry`,
+        username: `@Twitter`,
+        created_at: `06-30-2022`,
+        age: 47
+    },
+    {
+        tweet: `Today, we had cabbage. A lot of cabbage. The farts are so horrid I can barely stand myself.`,
+        username: `@Random_chick`,
+        created_at: `06-21-2022`,
+        age: 26
+    },
+    {
+        tweet: `Today, I found out that my roommate has a good voice, because he loudly sings Korean love songs at one in the morning.`,
+        username: `@Meh`,
+        created_at: `06-06-2022`,
+        age: 34
+    },
+    {
+        tweet: `Today, I caught a cold. Turns out that the neighbours who have noisy children have also caught COVID and have to isolate for a week. I not only have to deal with a cold, but I have to overhear kids shows and crying through the thin wall dividing our houses.`,
+        username: `@Jeanette`,
+        created_at: `06-21-2022`,
+        age: 20
+    },
+    {
+        tweet: `When you sleep over at your friends house and they forget to give you a blanket`,
+        username: `@tracceeeee`,
+        created_at: `07-28-2019`,
+        age: 26
+    },
+    {
+        tweet: `So wtf am I doing wrong`,
+        username: `@liyahhh_liyah`,
+        created_at: `11-29-2017`,
+        age: 17
+    },
+    {
+        tweet: `I put my life on the line so my girl can have the perfect photo LMFAO!!!`,
+        username: `@NATEGAWD`,
+        created_at: `03-30-2019`,
+        age: 16
+    },
     {
         tweet: `if you've ever stolen a Tweet say sorry`,
         username: `Twitter`,
         created_at: `06-30-2022`,
-        age: 16
+        age: 25
+    },
+    {
+        tweet: `if you've ever stolen a Tweet say sorry`,
+        username: `Twitter`,
+        created_at: `06-30-2022`,
+        age: 11
+    },
+    {
+        tweet: `if you've ever stolen a Tweet say sorry`,
+        username: `Twitter`,
+        created_at: `06-30-2022`,
+        age: 19
     },
 ]
